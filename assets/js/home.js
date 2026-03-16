f8013efd62df6208ab9306ec602611e47ea4a945
dmartinsid <deivid.martins@itau-unibanco.com.br>
2026-03-16 00:11:25 +0000

    Deploying to release from @ insanedriver/insanedriver.github.io@fa7d3c13f16e0dc3816f204e7cdada93ad9343d4 🚀

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,5 @@
 $(window).load(function () {
-    $("#player_spotify .follow_title").append('<h4><iframe src="https://embed.spotify.com/follow/1/?uri=spotify:artist:2ye4AQnKI7zNyOnuLGHaaE&amp;size=basic&amp;theme=dark" width="150" height="25" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowtransparency="true"></iframe></h4>');
-    $("#player_spotify .player_container").append('<iframe src="https://open.spotify.com/embed?uri=spotify:playlist:4NSGSicKX7WP3qrBLCUdkA" height="328" width="480" frameborder="0" allowtransparency="true" allow="encrypted-media" class="iframe-spotify" </iframe>');
+    $("#player_spotify .player_container").append('<iframe src="https://open.spotify.com/embed?uri=spotify:playlist:4NSGSicKX7WP3qrBLCUdkA" style="min-height: 400px;" width="100%" height="100%" frameborder="0" allowtransparency="true" allow="encrypted-media" class="iframe-spotify"></iframe>');
     window.loadingScreen.finish();
 });
 $(document).ready(function () {
